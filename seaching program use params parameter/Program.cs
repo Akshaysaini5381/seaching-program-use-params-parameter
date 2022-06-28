4241759094fb6ef201b5dc035e1fdc2cef13d975
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 04:48:06 +0530

    Add project files.

---
+++ seaching program use params parameter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace seaching_program_use_params_parameter
+{
+    class Program
+    {
+        static void show(int no,params int []arr)
+        {
+            bool flag = false;
+            int ctr = 0;
+            while (ctr<arr.Length)
+            {
+                if (arr[ctr]==no)
+                {
+                    flag = true;
+                  
+                    break;
+                  
+                }
+                ctr = ctr + 1;
+            }
+            if (flag==true)
+            {
+                Console.WriteLine("yes :");
+            }
+            else
+            {
+                Console.WriteLine("Not Found :");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            show(8,1,2,3,4,8,9);
+            Console.WriteLine("=======");
+            show(10, 1, 2, 3, 4, 8, 9);
+        }
+    }
+}
